9e29ebd1a8fb9df4cc8a206a05989ef19ed14133
clkbug <mail@clkbug.net>
2024-10-06 15:11:43 +0900

    update run.fsx

--- 01.awklike/run.fsx
+++ 01.awklike/run.fsx
@@ -1,34 +1,36 @@
 open System
 open System.IO
 
-[<EntryPoint>]
-let main argv =
-    if argv.Length < 1 then
-        printfn "Usage: <program> <filename>"
+
+let argv = fsi.CommandLineArgs
+
+if argv.Length < 1 then
+    printfn "Usage: <program> <filename>"
+    1
+else
+    let filename = argv.[1]
+    let mutable sum = int64 0
+
+    try
+        use reader = new StreamReader(filename)
+        let mutable lineNumber = 0
+
+        while not reader.EndOfStream do
+            let line = reader.ReadLine()
+
+            if lineNumber > 0 then
+                let numbers = line.Split('\t') |> Array.map int64 |> Array.sum
+                // let numbers = line.Split('\t') |> Array.fold (fun acc x -> acc + int64 x) (int64 0)
+                sum <- sum + numbers
+
+            lineNumber <- lineNumber + 1
+
+        printfn "%A" sum
+        0
+    with
+    | :? FileNotFoundException ->
+        printfn "File not found: %s" filename
         1
-    else
-        let filename = argv.[0]
-        let mutable sum = 0
-
-        try
-            use reader = new StreamReader(filename)
-            let mutable lineNumber = 0
-
-            while not reader.EndOfStream do
-                let line = reader.ReadLine()
-
-                if lineNumber > 0 then
-                    let numbers = line.Split('\t') |> Array.map int
-                    sum <- sum + (Array.sum numbers)
-
-                lineNumber <- lineNumber + 1
-
-            printfn "%d" sum
-            0
-        with
-        | :? FileNotFoundException ->
-            printfn "File not found: %s" filename
-            1
-        | ex ->
-            printfn "An error occurred: %s" (ex.Message)
-            1
+    | ex ->
+        printfn "An error occurred: %s" (ex.Message)
+        1
